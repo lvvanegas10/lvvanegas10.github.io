b9bd795a4745d2fe5a99b6b6bc54f4cd1bf105d5
Ricardo Andres Angel Villadiego <ra.angel10@uniandes.edu.co>
2018-08-28 10:21:30 -0500

    Update animations.js

--- javascript/animations.js
+++ javascript/animations.js
@@ -1,36 +1,37 @@
+var w = window;
 var TxtRotate = function(el, toRotate, period) {
-  this.toRotate = toRotate;
-  this.el = el;
-  this.loopNum = 0;
-  this.period = parseInt(period, 10) || 2000;
-  this.txt = '';
-  this.tick();
-  this.isDeleting = false;
+  w.toRotate = toRotate;
+  w.el = el;
+  w.loopNum = 0;
+  w.period = parseInt(period, 10) || 2000;
+  w.txt = '';
+  w.tick();
+  w.isDeleting = false;
 };
 
 TxtRotate.prototype.tick = function() {
-  var i = this.loopNum % this.toRotate.length;
-  var fullTxt = this.toRotate[i];
+  var i = w.loopNum % w.toRotate.length;
+  var fullTxt = w.toRotate[i];
 
-  if (this.isDeleting) {
-    this.txt = fullTxt.substring(0, this.txt.length - 1);
+  if (w.isDeleting) {
+    w.txt = fullTxt.substring(0, w.txt.length - 1);
   } else {
-    this.txt = fullTxt.substring(0, this.txt.length + 1);
+    w.txt = fullTxt.substring(0, w.txt.length + 1);
   }
 
-  this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+  this.el.innerHTML = '<span class="wrap">'+w.txt+'</span>';
 
-  var that = this;
+  var that = w;
   var delta = 300 - Math.random() * 100;
 
-  if (this.isDeleting) { delta /= 2; }
+  if (w.isDeleting) { delta /= 2; }
 
-  if (!this.isDeleting && this.txt === fullTxt) {
-    delta = this.period;
+  if (!w.isDeleting && w.txt === fullTxt) {
+    delta = w.period;
     this.isDeleting = true;
-  } else if (this.isDeleting && this.txt === '') {
-    this.isDeleting = false;
-    this.loopNum++;
+  } else if (w.isDeleting && w.txt === '') {
+    w.isDeleting = false;
+    w.loopNum++;
     delta = 500;
   }
 
